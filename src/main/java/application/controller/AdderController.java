6c9cd303ca290785e77194b585d442a237ca7a12
ghaniturismo <ghaniturismo@gmail.com>
2018-12-07 13:54:32 +0100

    ADD

---
+++ src/main/java/application/controller/AdderController.java
@@ -0,0 +1,25 @@
+package application.controller;
+
+import application.service.AdderService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping(value = "/adder", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+
+@RestController
+public class AdderController {
+    private AdderService adderService;
+    public AdderController(AdderService adderService) {
+        this.adderService = adderService;
+    }
+
+    @GetMapping("/current")
+    public int currentNum() {
+        return adderService.currentBase();
+    }
+
+    @PostMapping
+    public int add(@RequestParam int num) {
+        return adderService.add(num);
+    }
+}
---
+++ src/main/java/application/service/AdderService.java
@@ -0,0 +1,24 @@
+package application.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdderService {
+    private int num;
+
+    public void baseNum(int base) {
+        this.num = base;
+    }
+
+    public int currentBase() {
+        return num;
+    }
+
+    public int add(int adder) {
+        return this.num + adder;
+    }
+
+    public int accumulate(int adder) {
+        return this.num += adder;
+    }
+}
