ecc20de73bd056b89041dc8f569867346857cc02
Laura-Weber <laura.weber57@laposte.net>
2018-12-13 15:33:34 +0100

    modif core

--- src/main/java/application/DemoApplication.java
+++ src/main/java/application/DemoApplication.java
@@ -2,12 +2,21 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/*").allowedOrigins("*");
+	}
+
 }
